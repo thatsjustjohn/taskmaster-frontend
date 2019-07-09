38c641cf5b5597bdff5ed45b387b823cc0d9751c
John Winters <jhnwin@amazon.com>
2019-07-09 12:46:37 -0700

    added an s

--- src/task.jsx
+++ src/task.jsx
@@ -8,7 +8,7 @@ function Task(props){
       <p id={props.data.id}>Status: {props.data.status} </p>
       {props.data.assignee ? <p>Assigned To: {props.data.assignee}</p> : null}
 
-      <form action={`${props.taskmasterBackend}/tasks/${props.data.id}/image`} method="post" enctype="multipart/form-data">
+      <form action={`${props.taskmasterBackend}/tasks/${props.data.id}/images`} method="post" enctype="multipart/form-data">
         <label>
           <span>Upload Image</span>
           <input name="file" type="file" />
